21d0b82074d06971c294d74345796745a9005785
Marcus Mu <chunkhang@gmail.com>
2018-05-23 12:23:36 +0800

    Separate data

--- app.py
+++ app.py
@@ -1,59 +1,25 @@
 from flask import Flask, redirect, url_for, request, jsonify
+from data import DATA
+
 
 app = Flask(__name__)
 
-EMAIL = 'teckminc@sunway.edu.my'
 
 @app.route('/', methods=['GET'])
 def index():
-    return redirect(url_for('api', email=EMAIL))
+    return redirect(url_for('api', email='teckminc@sunway.edu.my'))
+
 
 @app.route('/classes', methods=['GET'])
 def api():
-    data = {
-        EMAIL: {
-            'semester': '2018-03-26',
-            'subjects': [
-                {
-                    'code': 'NET3204',
-                    'classes': [
-                        {
-                            'category': 'Lecture',
-                            'group': 1,
-                            'students': [
-                                '15011909',
-                                '10023222',
-                                '14099916'
-                            ]
-                        },
-                        {
-                            'category': 'Practical',
-                            'group': 1,
-                            'students': [
-                                '15011909',
-                                '10023222'
-                            ]
-                        },
-                        {
-                            'category': 'Practical',
-                            'group': 2,
-                            'students': [
-                                '14099916'
-                            ]
-                        }
-                    ]
-                }
-            ]
-        }
-    }
     email = request.args.get('email')
     semester = ''
     subjects = []
     if email is None:
         email = ''
-    if email in data:
-        semester = data[email]['semester']
-        subjects = data[email]['subjects']
+    if email in DATA:
+        semester = DATA[email]['semester']
+        subjects = DATA[email]['subjects']
     return jsonify({
         'email': email,
         'semester': semester,
---
+++ data.py
@@ -0,0 +1,36 @@
+DATA = {
+    'teckminc@sunway.edu.my': {
+        'semester': '2018-03-26',
+        'subjects': [
+            {
+                'code': 'NET3204',
+                'classes': [
+                    {
+                        'category': 'Lecture',
+                        'group': 1,
+                        'students': [
+                            '15011909',
+                            '10023222',
+                            '14099916'
+                        ]
+                    },
+                    {
+                        'category': 'Practical',
+                        'group': 1,
+                        'students': [
+                            '15011909',
+                            '10023222'
+                        ]
+                    },
+                    {
+                        'category': 'Practical',
+                        'group': 2,
+                        'students': [
+                            '14099916'
+                        ]
+                    }
+                ]
+            }
+        ]
+    }
+}
